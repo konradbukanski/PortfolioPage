2330cd991b77cfd72f93f46e3aeb8c33dbc68fbf
konradbukanski <konradbukanski@gmail.com>
2019-04-04 14:13:31 +0200

    project section animated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const aboutText = document.querySelector(".aboutme__content__info--text");
 const aboutImage = document.querySelector(".aboutme__content--photo--shape");
 const aboutCloud = document.querySelectorAll(".aboutme__cloud p span");
+const projectsItems = document.querySelector(".projects__items");
 
 const aboutme = document.querySelector(".aboutme");
 const about = document.querySelector(".about");
@@ -13,5 +14,9 @@ document.addEventListener("scroll", () => {
     aboutCloud.forEach(element => {
       element.classList.add("progres");
     });
+  }
+  if (scrollY > (about.scrollHeight + aboutme.scrollHeight) / 1.1) {
+    console.log("siema");
+    projectsItems.classList.add("moveTop");
   }
 });
