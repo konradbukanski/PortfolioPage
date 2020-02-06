49b21cb92e4e49b090252ecc27ee2b3db7e2155f
konradbukanski <46792943+konradbukanski@users.noreply.github.com>
2020-02-06 15:02:18 +0100

    menu scroll color

--- scroll.js
+++ scroll.js
@@ -31,3 +31,41 @@ document.querySelector(".contactme").addEventListener("click", () => {
         behavior: 'smooth' 
     })
 })
+
+
+const aboutmeHeight = document.querySelector(".aboutme").offsetHeight
+const skillsHeight = document.querySelector(".skills").offsetHeight
+const projectsHeight = document.querySelector(".projects").offsetHeight
+const contactHeight = document.querySelector(".contact").offsetHeight
+
+
+document.addEventListener("scroll", () => {
+    let scroll = document.documentElement.scrollTop
+    console.log()
+    if( scroll <= aboutmeHeight-aboutmeHeight/2){
+        document.querySelector(".home").style.color = "#f84545"
+    }else{
+        document.querySelector(".home").style.color = "#cccccc"
+    }
+    if( scroll < aboutmeHeight+skillsHeight - aboutmeHeight/2 && scroll > aboutmeHeight - aboutmeHeight/2){
+        document.querySelector(".user").style.color = "#f84545"
+    }else{
+        document.querySelector(".user").style.color = "#cccccc"
+    }
+    if( scroll < aboutmeHeight+skillsHeight+projectsHeight - aboutmeHeight/2 && scroll > (aboutmeHeight + skillsHeight - aboutmeHeight/2)){
+        document.querySelector(".skill").style.color = "#f84545"
+    }else{
+        document.querySelector(".skill").style.color = "#cccccc"
+    }
+    if( scroll < aboutmeHeight+skillsHeight+projectsHeight+contactHeight-aboutmeHeight/2 && scroll > (aboutmeHeight + skillsHeight + projectsHeight - aboutmeHeight/2)){
+        document.querySelector(".project").style.color = "#f84545"
+    }else{
+        document.querySelector(".project").style.color = "#cccccc"
+    }
+    if( scroll > (aboutmeHeight + skillsHeight + projectsHeight + contactHeight - aboutmeHeight/2)){
+        document.querySelector(".contactme").style.color = "#f84545"
+    }else{
+        document.querySelector(".contactme").style.color = "#cccccc"
+    }
+    
+})
